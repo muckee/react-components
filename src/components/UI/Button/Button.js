fe5257588dce5c58c9eee3fdad716e82ec35e8bb
Muckee <info@joshuaflood.com>
2023-05-14 15:12:10 +0100

    Extend button styles

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,9 +3,27 @@ import React from 'react';
 import styles from './Button.module.css';
 
 const Button = props => {
+
+  let className = styles.button;
+
+  if(props.className) {
+    className += ' ' + props.className;
+  }
+
+  if(props.status) {
+    // Primary
+    // Secondary
+    // Text
+    // Success
+    // Warning
+    // Danger
+    // Info
+    className += ' ' + props.status;
+  }
+
   return <button
     type={props.type || 'button'}
-    className={`${styles.button} ${props.className ? props.className : ''}`}
+    className={`${styles.button}${props.className ? ' ' + props.className : ''}`}
     onClick={props.onClick}
     onMouseDown={props.onMouseDown}
     onMouseUp={props.onMouseUp}

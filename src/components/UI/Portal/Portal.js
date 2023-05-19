2316db15ee22517f7eaa23f45771e845409d00e3
Muckee <info@joshuaflood.com>
2023-05-19 18:56:31 +0100

    Add dynamic className to portal

--- src/components/UI/Portal/Portal.js
+++ src/components/UI/Portal/Portal.js
@@ -5,11 +5,15 @@ const Portal = props => {
 
     const {
         id,
+        className,
         children,
     } = props;
 
     const rootElement = useRef(document.getElementById(id) || document.createElement('div'));
 
+    if(className) {
+        rootElement.current.classList.add(className);
+    }
     const [isDynamic] = useState(!rootElement.current.parentElement);
 
     useEffect(() => {
